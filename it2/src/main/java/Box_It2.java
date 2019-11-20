1d990f8afc467fe0a98fc2b779407a9906b9344c
Enrique Villarrubia <enriqueadrian.villarrubia@alu.uclm.es>
2019-11-20 22:45:37 +0100

    Add iteration 3

--- it2/src/main/java/Box_It2.java
+++
@@ -1,2 +0,0 @@
-public class Box_It2 {
-}
---
+++ it3/src/main/java/Cirebytes/CarWorkshopMSProject/Domain/PartList.java
@@ -0,0 +1,25 @@
+package Cirebytes.CarWorkshopMSProject.Domain;
+
+import java.util.List;
+
+public class PartList {
+
+	private List<String> partList;
+	private Boolean isNeeded;
+
+	public void createPartList() {
+		// TODO - implement PartList.createPartList
+		throw new UnsupportedOperationException();
+	}
+
+	public void createNeededPartList() {
+		// TODO - implement PartList.createNeededPartList
+		throw new UnsupportedOperationException();
+	}
+
+	public void checkAvailability() {
+		// TODO - implement PartList.checkAvailability
+		throw new UnsupportedOperationException();
+	}
+
+}
